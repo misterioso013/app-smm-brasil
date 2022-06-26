21cae03ab8606ae84fb6270f9d85f2fa67fccf96
Rosiel Victor <codepremium917@gmail.com>
2022-06-26 06:03:16 -0300

    Version Beta

--- App.js
+++ App.js
@@ -1,20 +1,54 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, BackHandler } from 'react-native';
+import {WebView} from 'react-native-webview';
+import { getStatusBarHeight } from 'react-native-status-bar-height';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      url: 'https://smm.app.br/auth/login',
+    };
+  }
+
+  componentDidMount() {
+    BackHandler.addEventListener('hardwareBackPress', this.handleBackButton);
+  }
+
+  componentWillUnmount() {
+    BackHandler.removeEventListener('hardwareBackPress', this.handleBackButton);
+  }
+
+  handleBackButton = () => {
+    this.setState({url: 'https://smm.app.br/statistics'});
+    return true;
+  }
+
+  render() {
+    return (
+      <WebView
+        source={{uri: this.state.url}}
+        style={styles.webView}
+        onNavigationStateChange={(event) => {
+          this.setState({url: event.url});
+        }
+        }
+        sharedCookiesEnabled={true}
+        cacheEnabled={true}
+        javaScriptEnabled={true}
+        domStorageEnabled={true}
+      />
+    );
+  }
 }
 
+
+
 const styles = StyleSheet.create({
-  container: {
+  webView: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: getStatusBarHeight(),
   },
 });
+
+export default App;
